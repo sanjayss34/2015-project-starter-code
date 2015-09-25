a9b48d6bcef807e95c88b01d712482f0b0829ad4
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2015-09-25 14:51:52 -0500

    Pieces roughly finished

--- app/models.py
+++ app/models.py
@@ -2,29 +2,37 @@ from . import db
 from datetime import datetime
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+class TreasurePoint(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+    #username = db.Column(db.String(80), unique=True)
+    #email = db.Column(db.String(120), unique=True)
+    #posts = db.relationship('Post', backref='author', lazy='dynamic')
+	latitude = db.Column(db.Float, unique=False)
+	longitude = db.Column(db.Float, unique=False)
+	notes = db.Column(db.String, unique=False)
+    #def __init__(self, username, email):
+	def __init__(self, latitude, longitude, notes):
+		#self.username = username
+        #self.email = email
+		self.latitude = latitude
+		self.longitude = longitude
+		self.notes = notes
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+	def __repr__(self):
+		return [self.latitude, self.longitude, self.notes]
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def is_cool(self):
-        return (self.username == 'aharelick')
+	#def is_cool(self):
+        #return (self.username == 'aharelick')
 
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    body = db.Column(db.Text)
-    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(80))
+	body = db.Column(db.Text)
+	pub_date = db.Column(db.DateTime, default=datetime.utcnow())
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __repr__(self):
-        return '<Post %r>' % self.title
+	def __repr__(self):
+		return '<Post %r>' % self.title
+
+
