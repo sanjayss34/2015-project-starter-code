39b20ef034948bb01d340dee4eae15e72d15b617
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2015-09-26 18:01:37 -0500

    made TreasurePoint lowercase

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from . import db
 from datetime import datetime
 
 
-class TreasurePoint(db.Model):
+class treasurepoint(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
     #username = db.Column(db.String(80), unique=True)
     #email = db.Column(db.String(120), unique=True)
